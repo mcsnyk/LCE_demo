50096f2c5c5156709b2d74465db8210325577912
mcsnyk <miklos.csecsi@snyk.io>
2023-03-04 21:10:51 +0100

    edited files

--- broker/broker.sh
+++ broker/broker.sh
@@ -4,9 +4,8 @@ docker network create ADDA_demo
 # Gitlab:
 docker run -d --name github-broker \
 -p 8001:8001 \
--e BROKER_TOKEN=1053ed9d-0503-46b2-be61-9abf36cbed6c \
--e GITHUB_TOKEN=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 \
--e GITHUB=github.com \
+-e BROKER_TOKEN=... \
+-e GITHUB_TOKEN=... \
 -e BROKER_CLIENT_URL=http://github-broker:8001 \
 -e GIT_CLIENT_URL=http://github-code-agent:3000 \
 -e ACCEPT=/home/node/private \
@@ -17,5 +16,5 @@ snyk/broker:github-com
 
 docker run -d --name github-code-agent \
 -p 3000:3000 \
--e PORT=3000 -e SNYK_TOKEN=aeeabf85-e8d9-4c0f-b7ba-ef1a1ef7c0e7 --network ADDA_demo \
+-e PORT=3000 -e SNYK_TOKEN=... --network ADDA_demo \
 snyk/code-agent
